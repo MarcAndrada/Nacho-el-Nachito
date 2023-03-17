9fc6f7663b2bd6749692171a2e6b82b3d9539973
Dani ENTI <daniel.munoz.jimenez@enti.cat>
2023-03-17 10:38:59 +0100

    Paredes destructibles implementadas

--- Assets/Scripts/Scenari/DestructibleWall/DestructibleWallLogic.cs
+++ Assets/Scripts/Scenari/DestructibleWall/DestructibleWallLogic.cs
@@ -6,14 +6,11 @@ public class DestructibleWallLogic : MonoBehaviour
 {
     private PlayerController _playerController;
 
-    private void Start()
-    {
-        _playerController = GetComponent<PlayerController>();
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag.Equals("Player") && _playerController.playerState == PlayerController.PlayerStates.HOOK)
+        _playerController = collision.GetComponent<PlayerController>();
+
+        if (collision.tag.Equals("Player") && _playerController.playerState == PlayerController.PlayerStates.HOOK)
         {
             Destroy(gameObject);
         }
