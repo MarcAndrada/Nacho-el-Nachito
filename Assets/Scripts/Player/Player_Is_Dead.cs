fd168d9f2af7b65b7005e38ccdb01e57bcf6d886
Gisela Benito Izquierdo <gisela.benito@emti.cat>
2023-03-21 13:41:53 +0100

    Player is dead casi acabado

--- Assets/Scripts/Player/Player_Is_Dead.cs
+++ Assets/Scripts/Player/Player_Is_Dead.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class Player_Is_Dead : MonoBehaviour
 {
-    [SerializeField] private int timeSprite; 
+    [SerializeField] private float timeSprite;
+
+    private bool isDead;
+    private float tiempo;
+    private SpriteRenderer dibujo;
+    private Transform posicion; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dibujo = GetComponent<SpriteRenderer>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        tiempo = timeSprite;
+
+        if (collision.CompareTag("Obstaculo"))
+        {
+            dibujo.enabled = false;
+            while (timeSprite > 0)
+            {
+                timeSprite -= timeSprite - 1f * Time.deltaTime;
+            }
+        }
+
+        if(timeSprite == 0)
+        {
+            dibujo.enabled = true;
+            timeSprite = tiempo; 
+        }
     }
 }
