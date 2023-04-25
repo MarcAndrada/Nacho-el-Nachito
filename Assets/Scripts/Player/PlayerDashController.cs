e644c36d172f24fc60bd06edf33121691594768b
Jordi Planas <jordi.planas@enti.cat>
2023-04-25 13:29:23 +0200

    Dash solucionado

--- Assets/Scripts/Player/PlayerDashController.cs
+++ Assets/Scripts/Player/PlayerDashController.cs
@@ -21,7 +21,8 @@ public class PlayerDashController : MonoBehaviour
     // DASH VARIABLES
 
     public bool _canDash;
-
+    private bool timeStopped;
+    
     private Vector2 vdirection;
     [HideInInspector] public  Vector2 _dashDirection;
     public bool _isDirectional;
@@ -29,11 +30,14 @@ public class PlayerDashController : MonoBehaviour
     [SerializeField] private float _dashSpeed = 28f;
     [SerializeField] private float _dashTime = 0.3f;
     private float _dashTimePassed = 0f;
-
+    public float capsuleOffset = 0.15F;
+    public float speedDashController = 7;
+    
     void Start()
     {
         _playerController = GetComponent<PlayerController>();
-        
+
+        timeStopped = false;
         rb2d = GetComponent<Rigidbody2D>();
         sprt = GetComponent<SpriteRenderer>();
         coll = GetComponent<CapsuleCollider2D>();
@@ -58,47 +62,62 @@ public class PlayerDashController : MonoBehaviour
 
     public void DashTimer()
     {
-        _dashTimePassed += Time.deltaTime;
-        if (_dashTimePassed >= _dashTime)
+        if (!timeStopped)
         {
-            float _ySpeed = Mathf.Clamp(rb2d.velocity.y, -5, 5);
-            rb2d.velocity = new Vector2(rb2d.velocity.x, _ySpeed);
-            _playerController.playerState = PlayerController.PlayerStates.NONE;
-            _dashTimePassed = 0;
+            _dashTimePassed += Time.deltaTime;
+            if (_dashTimePassed >= _dashTime)
+            {
+                StopDash();
+            }
         }
+        
     }
 
     public void DashCheckWall()
     {
-        if (Physics2D.Raycast(transform.position, transform.right * _dashDirection.x, 1, floorLayer))
+        
+        bool hitUp = Physics2D.Raycast(transform.position + new Vector3(0, coll.size.y/2f + capsuleOffset), transform.right * _dashDirection.x, 0.55f, floorLayer);
+        bool hitDown = Physics2D.Raycast(transform.position - new Vector3(0, coll.size.y/2 + capsuleOffset), transform.right * _dashDirection.x, 0.55f, floorLayer);
+
+        if (hitUp && hitDown)
         {
-            // Stop Dash
+            StopDash();
+        }
+        else if (hitUp)
+        {
+            timeStopped = true;
+            rb2d.velocity += new Vector2(0, - speedDashController);
+        }
+        else if (hitDown)
+        {
+            timeStopped = true;
+            rb2d.velocity += new Vector2(0,  speedDashController);
         }
         else
         {
-            bool hitUp = Physics2D.Raycast(transform.position + new Vector3(0, 0.55f), transform.right * _dashDirection.x + new Vector3(0.5f, 0.55f), 1, floorLayer);
-            bool hitDown = Physics2D.Raycast(transform.position + new Vector3(0, 0.55f), transform.right * _dashDirection.x + new Vector3(0.5f, -0.55f), 1, floorLayer);
-
-            if (hitUp && hitDown)
-            {
-                // Stop Dash
-            }
-            else if (hitUp)
-            {
-                // Move Down                
-            }
-            else if (hitDown)
-            {
-                // Move Up
-            }
+            timeStopped = false;
         }
+        
     }
+
+    private void StopDash()
+    {
+        float _ySpeed = Mathf.Clamp(vdirection.y * _dashSpeed, -5, 5);
+        rb2d.velocity = new Vector2(rb2d.velocity.x, _ySpeed);
+        _playerController.playerState = PlayerController.PlayerStates.NONE;
+        _dashTimePassed = 0;
+    }
+    
+    
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position + new Vector3(0, 0.55f) , transform.position + new Vector3(0, 0.55f) + transform.right * _dashDirection.x);
-        Gizmos.DrawLine(transform.position , transform.position + transform.right * _dashDirection.x);
-        Gizmos.DrawLine(transform.position + new Vector3(0, -0.55f) , transform.position + new Vector3(0, -0.55f) + transform.right * _dashDirection.x);
+        if (coll != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position + new Vector3(0, coll.size.y/2 + capsuleOffset) , transform.position + new Vector3(0, coll.size.y/2 + capsuleOffset) + transform.right * _dashDirection.x);
+            Gizmos.DrawLine(transform.position , transform.position + transform.right * _dashDirection.x);
+            Gizmos.DrawLine(transform.position + new Vector3(0, -coll.size.y/2 - capsuleOffset) , transform.position + new Vector3(0, -coll.size.y/2f - capsuleOffset) + transform.right * _dashDirection.x);
+        }
     }
 }
