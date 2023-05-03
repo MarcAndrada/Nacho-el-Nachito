fae18a445892d1d6ac6fe80f783cd041c7a11326
Dani ENTI <daniel.munoz.jimenez@enti.cat>
2023-05-03 10:44:15 +0200

    Menu de pausa y master volume implementados

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -24,6 +24,11 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene(GameScene);
     }
 
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+    }
+
     public void CloseGame()
     {
         Application.Quit();
